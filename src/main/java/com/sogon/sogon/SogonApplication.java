5fd3bb90c88dc9353f59d7824055157fab2f7ac3
no-merit <128347576+no-merit@users.noreply.github.com>
2025-04-13 23:41:04 +0900

    Project Init

---
+++ src/main/java/com/sogon/sogon/SogonApplication.java
@@ -0,0 +1,13 @@
+package com.sogon.sogon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SogonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SogonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sogon/sogon/SogonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sogon.sogon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SogonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
